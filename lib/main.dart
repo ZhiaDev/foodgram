f4fd8a1e46a2f7bbf1bd6bf4c3e696433630a911
Parsa Sharifi <sharifidev@outlook.com>
2022-11-15 20:40:56 +0330

    splash screen is complete

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,47 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF14151C),
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage('images/splash_screen/pattern.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Center(
+              child: Image(image: AssetImage('images/splash_screen/logo.png')),
+            ),
+
+            Positioned(
+              bottom: 41,
+              child: Column(
+                children: [
+                  Text('From', style: TextStyle(color: Color(0xFF7A7A7D)),),
+                  Text('Cafe Flutter', style: TextStyle(color: Color(0xFF5263FC)),),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
