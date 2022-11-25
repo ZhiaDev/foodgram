722ac2882741560802d7e41f457838286d97c0d3
Parsa Sharifi <sharifidev@outlook.com>
2022-11-26 00:05:50 +0330

    create a custom add-story icon button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF14151C),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(17),
+            width: 64,
+            height: 64,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(17),
+              border: Border.all(color: Colors.white, width: 2)
+            ),
+            child: SvgPicture.asset('assets/images/home_screen/plus.svg'),
+          ),
+        ),
+      ),
+    );
   }
 }
