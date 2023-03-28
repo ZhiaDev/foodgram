a1221ea33825f003ea5d56d4011bfea705076405
Parsa Sharifi <sharifidev@outlook.com>
2023-03-28 23:04:58 +0330

    Move the splash screen to a separate file from the main file

---
+++ lib/screens/splash.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF14151C),
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage('assets/images/splash_screen/pattern.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Center(
+              child: SvgPicture.asset('assets/images/splash_screen/logo.svg'),
+            ),
+            Positioned(
+              bottom: 41,
+              child: Column(
+                children: [
+                  Text(
+                    'by',
+                    style: GoogleFonts.outfit(
+                      textStyle: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFFFFFFFF).withOpacity(0.6),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'KylixDev',
+                    style: GoogleFonts.outfit(
+                      textStyle: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF5263FC),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
