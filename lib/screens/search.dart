b5a26b8c0275e22f267d4e68d172527faf1e3621
Parsa Sharifi <sharifidev@outlook.com>
2023-04-19 09:00:54 +0330

    complete categories and Explore Posts: Search screen is fuly completed

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -10,98 +11,145 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF14151C),
       body: SafeArea(
-        child: Column(
-          children: [
-            _getSearchBox(),
-            _getCategories(context),
-          ],
-        ),
-      ),
+          child: CustomScrollView(
+        slivers: [_getSearchBox(), _getCategories(context), _getExplore()],
+      )),
     );
   }
 
-  Widget _getSearchBox() {
-    return Container(
-      margin: EdgeInsets.only(right: 16, left: 16, top: 8),
-      height: 46,
-      decoration: BoxDecoration(
-        color: Color(0xFF202232),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 14,
-          vertical: 4,
+  SliverToBoxAdapter _getSearchBox() {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: EdgeInsets.only(right: 16, left: 16, top: 8),
+        height: 46,
+        decoration: BoxDecoration(
+          color: Color(0xFF202232),
+          borderRadius: BorderRadius.circular(12),
         ),
-        child: Row(
-          children: [
-            SvgPicture.asset(
-              'assets/icons/search.svg',
-              height: 24,
-            ),
-            SizedBox(width: 8),
-            Expanded(
-              child: TextField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 14,
+            vertical: 4,
+          ),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                'assets/icons/search.svg',
+                height: 24,
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 14),
-            SvgPicture.asset(
-              'assets/icons/scan.svg',
-              height: 24,
-            ),
-          ],
+              SizedBox(width: 14),
+              SvgPicture.asset(
+                'assets/icons/scan.svg',
+                height: 24,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _getCategories(context) {
-    return Container(
-      height: 28,
-      margin: EdgeInsets.only(top: 20, bottom: 20),
-      child: ListView.separated(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemCount: 12, // ! hc
-        itemBuilder: (BuildContext context, int index) {
-          return SizedBox(
-            width: 16,
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF202232),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  'category k.$index',
-                  style: GoogleFonts.outfit(
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
+  SliverToBoxAdapter _getCategories(context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 28,
+        margin: EdgeInsets.only(top: 20, bottom: 20),
+        child: ListView.separated(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: 12, // ! hc
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              width: 16,
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF202232),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'category k.$index',
+                    style: GoogleFonts.outfit(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
 
-  
+  Widget _getExplore() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Container(
+              child: ClipRRect(
+                child: Image.asset(
+                  'assets/posts/post-$index.png',
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFF202232),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            );
+          },
+          childCount: 24,
+        ),
+        gridDelegate: SliverQuiltedGridDelegate(
+          crossAxisCount: 3,
+          mainAxisSpacing: 6,
+          crossAxisSpacing: 6,
+          pattern: [
+            QuiltedGridTile(2, 1),
+            QuiltedGridTile(2, 2),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(2, 2),
+            QuiltedGridTile(2, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+          ],
+        ),
+      ),
+    );
+  }
 }
