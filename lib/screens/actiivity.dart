d69b58fce7dff8e2aae738aed535d82cf73f62a7
Parsa Sharifi <sharifidev@outlook.com>
2023-04-19 15:33:21 +0330

    create ActivityScreen

---
+++ lib/screens/actiivity.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatefulWidget {
+  const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF14151C),
+      body: Center(
+        child: Text('HELLO'),
+      ),
+    );
+  }
+}
