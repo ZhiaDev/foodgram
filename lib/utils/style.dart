31ea65baf5dbf504dcd170f0edfc1225f7a132f3
Parsa Sharifi <sharifidev@outlook.com>
2023-05-09 21:23:05 +0330

    create Style file theme

---
+++ lib/utils/style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  const MyColors();
+
+  static const Color black1 = Color(0xff000000);
+  static const Color primery = Color(0xffe5383b);
+  static const Color red1 = Color(0xff660708);
+  static const Color red2 = Color(0xffa4161a);
+  static const Color red3 = Color(0xffba181b);
+  static const Color grey1 = Color(0xff8c8c8c);
+  static const Color grey2 = Color(0xffb3b3b3);
+  static const Color grey3 = Color(0xffd3d3d3);
+  static const Color grey4 = Color(0xfff5f3f4);
+  static const Color white = Color(0xffffffff);
+  static const Color link = Color(0xff1a96f0);
+
+}
+
+
+
+class FigmaTextStyles {
+  const FigmaTextStyles();
+
+
+}
