74cd3b358f277bbc7f692e0a61cae3d9e3d45567
Parsa Sharifi <sharifidev@outlook.com>
2023-05-18 18:20:37 +0330

    create Profile Screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Text('TTT'),
+        ),
+      ),
+    );
+  }
+}
